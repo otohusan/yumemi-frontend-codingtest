ca03100f4936e9949e43e20bf4257c67701d1d2d
otohusan <tamioguitar@gmail.com>
2023-11-12 16:04:21 +0900

    feat: URLとAPIKEYを利用して都道府県情報を取得するためにカスタムフックを作成

---
+++ src/hooks/useGetData.tsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function useGetData(url: string, apiKey: string): any {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get(url, {
+        headers: {
+          'X-API-KEY': apiKey,
+        },
+      })
+      .then((response) => {
+        setData(response.data.result);
+        setLoading(false);
+      })
+      .catch((error) => {
+        alert(`${error}が起きました`);
+        setLoading(false);
+      });
+  }, []);
+
+  return [data, loading];
+}
+
+export default useGetData;
--- src/pages/PopulationGraph.tsx
+++ src/pages/PopulationGraph.tsx
@@ -1,9 +1,25 @@
+import { type Key } from 'react';
 import PrefectureRadioButtons from '../features/PopulationGraph/components/PrefectureRadioButtons';
+import useGetData from '../hooks/useGetData';
 
 function PopulationGraph(): JSX.Element {
+  // apiKeyを環境変数から取得
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const [prefectureData, prefectureDataLoding] = useGetData(
+    'https://opendata.resas-portal.go.jp/api/v1/prefectures',
+    apiKey
+  );
+
   return (
     <div>
       <PrefectureRadioButtons />
+      <ul>
+        {prefectureData.map(
+          (prefecture: { prefCode: Key; prefName: string }) => (
+            <li key={prefecture.prefCode}>{prefecture.prefName}</li>
+          )
+        )}
+      </ul>
     </div>
   );
 }

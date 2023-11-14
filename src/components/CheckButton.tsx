04e864f346c6e1f46459d3f5450d411f133bc476
otohusan <tamioguitar@gmail.com>
2023-11-14 09:49:35 +0900

    feat: 都道府県選択のためのcheckButtonを追加

---
+++ src/components/CheckButton.tsx
@@ -0,0 +1,34 @@
+interface CheckButtonProps {
+  option: string;
+  checkedValues: string;
+  // 何かデータを追加したい時に使える
+  dataSet: number;
+  //   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function CheckButton({
+  option,
+  checkedValues,
+  dataSet,
+} //   onChange,
+: CheckButtonProps): JSX.Element {
+  function demoFunc(): void {}
+
+  return (
+    <div>
+      <label key={option}>
+        <input
+          type='checkbox'
+          value={option}
+          // セレクトボックスがチェックされた、またはチェックが外れた際に関数handleChangeが実行される
+          onChange={demoFunc}
+          data-code={dataSet}
+          checked={checkedValues.includes(option)}
+        />
+        {option}
+      </label>
+    </div>
+  );
+}
+
+export default CheckButton;
